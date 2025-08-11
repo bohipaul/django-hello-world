7da0911bdc07a531f91ea4b6b8e1fc69ad0f289d
bohipaul <jarvisse@gmail.com>
2025-08-11 20:24:38 +0100

    Add prettify-min.js for code syntax highlighting in REST framework

--- api/settings.py
+++ api/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Whitenoise configuration for static file serving
+if not DEBUG:
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
